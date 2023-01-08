dd6fbf2f213d374451ffa2ba7288fa2fbdd79de8
altunog <altunog@gmail.com>
2023-01-08 16:40:02 +0300

    organize and rearrange files and folders

---
+++ GithubFollowers/GithubFollowers/Custom Views/Views/GFEmptyStateView.swift
@@ -0,0 +1,20 @@
+//
+//  GFEmptyStateView.swift
+//  GithubFollowers
+//
+//  Created by Oğuz Kaan Altun on 8.01.2023.
+//
+
+import UIKit
+
+class GFEmptyStateView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
