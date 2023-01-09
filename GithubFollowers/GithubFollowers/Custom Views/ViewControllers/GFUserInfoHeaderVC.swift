0d8c095857ab69a26a23cebd1306ff15e361f6f6
altunog <altunog@gmail.com>
2023-01-09 23:56:05 +0300

    create secondary title label custom component

---
+++ GithubFollowers/GithubFollowers/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,36 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GithubFollowers
+//
+//  Created by Oğuz Kaan Altun on 9.01.2023.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .left, fontSize: 34)
+    let nameLabel = GFSecondaryTitleLabel(fontSize: 18)
+    let locationImageView = UIImageView()
+    let locationLabel = GFSecondaryTitleLabel(fontSize: 18)
+    let bioLabel = GFBodyLabel(textAlignment: .left)
+    
+    var user: User
+    
+    init(user: User) {
+        self.user = user
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
