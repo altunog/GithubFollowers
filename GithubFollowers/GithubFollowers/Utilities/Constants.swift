24d6954fd84377128a787ae03b281ce0a9b81182
altunog <altunog@gmail.com>
2023-01-14 12:25:48 +0300

    create GFUserInfoHeaderVC, configure UserInfoVC

---
+++ GithubFollowers/GithubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GithubFollowers
+//
+//  Created by Oğuz Kaan Altun on 14.01.2023.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
